8354d23ad77502b429a68acad54850d8a6284553
atique <atique@d9a0bbc3-6d19-d345-a846-96ab8b4ebfc9>
2014-02-12 07:35:55 +0000

    correction of broken links and lots of refactoring, files renamed

--- SAOS Main App/asp.net-code/SAOS_Main_VS/Properties/AssemblyInfo.cs
+++ SAOS Main App/asp.net-code/SAOS_Main_VS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SAOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Special build by Saint Atique")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SAOS")]
 [assembly: AssemblyProduct("SAOS")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
