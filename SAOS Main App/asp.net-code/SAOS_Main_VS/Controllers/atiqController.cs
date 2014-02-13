21a31b891bbdd6982d22864ac0cf4cf22f5da6c5
atique <atique@d9a0bbc3-6d19-d345-a846-96ab8b4ebfc9>
2014-02-13 11:35:17 +0000

    all minor requirements met, for release as SAOS c 2014

---
+++ SAOS Main App/asp.net-code/SAOS_Main_VS/Controllers/atiqController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAOS.Controllers
+{
+    public class atiqController : Controller
+    {
+        //
+        // GET: /atiq/
+
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public string Index()
+        {
+            return "<html>"+
+                "<title>"+
+                "Resume of Md. Atiqur Rahman"+
+                "</title>"+
+                "<body><p align=\"center\"><a href=\"http://saos.co.in/Downloads/SA_Resume_latex.pdf\">Download original</a><br /><br />" +
+                "<iframe src="+"http://docs.google.com/viewer?url=http%3A%2F%2Fsaos.co.in%2FDownloads%2FSA_Resume_latex.pdf&embedded=true"+
+                " width=\"1024\" height=\"3900\" style=\"border: none;\"></iframe></p></body></html>";
+        }
+    }
+}
