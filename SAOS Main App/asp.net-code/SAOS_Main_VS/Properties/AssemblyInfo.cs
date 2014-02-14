18016458b720b6bab35ca81ef589a4a12a61f74c
atique <atique@d9a0bbc3-6d19-d345-a846-96ab8b4ebfc9>
2014-02-14 17:30:35 +0000

    favicon fixed, versions updated in assembly and in layout, files are properly put in revision as of r9

--- SAOS Main App/asp.net-code/SAOS_Main_VS/Properties/AssemblyInfo.cs
+++ SAOS Main App/asp.net-code/SAOS_Main_VS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVC4Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SAOS")]
+[assembly: AssemblyDescription("SAOS Cloud App")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MVC4Test")]
+[assembly: AssemblyCompany("SAOS")]
+[assembly: AssemblyProduct("SAOS Managed App")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
