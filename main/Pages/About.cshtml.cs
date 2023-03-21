d22fad89fed51f2c1ba7a1f7121de77182094036
Atiq Rahman <atiqcx@gmail.com>
2023-03-21 11:58:38 -0700

    Razor WebApp - Add About and Contact Pages

---
+++ main/Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Main.Pages;
+
+public class AboutModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
